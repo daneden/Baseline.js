f8e1840d1177a08884657c2df38d40a29c1bba71
Michael Fields <michael@mfields.org>
2012-08-30 03:49:45 -0700

    Set the value of this.target in init() before setBase() is called.

--- baseline.js
+++ baseline.js
@@ -23,6 +23,7 @@ var baseline = function(){
 
 		init: function(selector, target){
 			if(selector === undefined || target === undefined) return false;
+			this.target = target;
 			this.images = document.querySelectorAll(selector);
 			this.setbase(this.images);
 			this.tellmetarget(target);
