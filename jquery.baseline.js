2485eebc47c7c297e06c6e32eacc2a0c40795ffa
Sacha Schmid <schmid.sacha@gmail.com>
2012-12-20 09:25:07 +0100

    Bugfixes
    Prevent images from getting `max-height: 0` and fix resize actions.

--- jquery.baseline.js
+++ jquery.baseline.js
@@ -6,48 +6,47 @@
 * Released under the WTFPL license
 * http://sam.zoy.org/wtfpl/
 *
-* Date: Wed June 20 11:39:00 2012 GMT
+* Date: Thu December 20 09:23:00 2012 GMT+1
 */
-(function( $ ) {
+(function ($) {
+    $.fn.baseline = function (breakpoints) {
+        // Set up our variables, like a good little developer
+        var tall, newHeight, base, old, current = 0;
 
-	$.fn.baseline = function(breakpoints) {
+        return this.each(function(){
+            var $this = $(this); // Set the images as objects
 
-		// Set up our variables, like a good little developer
-		var tall, newHeight, base, old = 0;
+            var setbase = function (breakpoints) {
+                // Check if a single value or multiple breakpoints are given
+                if (typeof breakpoints === 'number') {
+                    base = breakpoints;
+                } else if (typeof breakpoints === 'object') {
+                    // Loop through the breakpoints and check which baseline to apply
+                    for (key in breakpoints) {
+                        if (breakpoints.hasOwnProperty(key)) {
+                            current = parseInt(key);
 
-		return this.each(function(){
-			var $this = $(this); // Set the images as objects
+                            if (document.width >= current && current >= old) {
+                                base = breakpoints[key];
+                                old = current;
+                            }
+                        }
+                    }
 
-			var setbase = function(breakpoints) { // The fun starts here
-			
-				// Check if a single value or multiple breakpoints are given                
-		                if (typeof breakpoints === 'number') {
-		                    base = breakpoints;
-		                } else if (typeof breakpoints === 'object') {
-		                    // Loop through the breakpoints and check which baseline to apply
-		                    for (key in breakpoints) {
-		                    	var current = parseInt(key);
-		                        if (document.width > current && current >= old) {
-		                            base = breakpoints[key];
-		                            old = current;
-		                        }
-		                    }
-		                }
-                
-				$this.css('maxHeight', 'none'); // Remove old max-height so that we can resize up as well as down
-				tall = $this.height(); // Grab the height
-				newHeight = Math.floor(tall / base) * base; // Make up a new height based on the baseline
-				$this.css('maxHeight', newHeight); // Set it!
-			}
+                    old = current = 0;
+                }
 
-			setbase(breakpoints); // Call on load
+                $this.css('maxHeight', 'none'); // Remove old max-height so that we can resize up as well as down
+                tall = $this.height(); // Grab the height
+                newHeight = Math.floor(tall / base) * base; // Make up a new height based on the baseline
+                $this.css('maxHeight', newHeight > 0 ? newHeight : 'none'); // Set it!
+            }
 
-			$(window).resize(function(){ // And call again on resize.
-				setbase(breakpoints);
-			});
+            setbase(breakpoints); // Call on load
 
-		});
-
-	}
-
-}) ( jQuery );
+            $(window).resize(function(){ // And call again on resize.
+                setbase(breakpoints);
+            });
+        });
+    }
+})(jQuery);
